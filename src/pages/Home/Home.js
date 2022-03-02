662f66b1e761f527ac9ae6fbd8a620b3f1ea8524
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2022-03-02 16:38:27 +0100

    preparing home styles

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { productList } from "../../apiCall";
-
+import { Container, Row, Col } from "react-bootstrap";
 import InputText from "../../components/InputText/InputText";
 function Home() {
+  const [items, setItems] = useState();
+  useEffect(() => {
+    productList().then((res) => console.log(res));
+  }, []);
   const [searcherData, setSearcherData] = useState({
     ProductSearcher: "",
   });
-  function products() {
-    productList().then((res) => console.log(res));
-  }
+
   function setSearcherWord(e) {
     // console.log();
     setSearcherData({
@@ -21,21 +23,26 @@ function Home() {
   const id = "soy un id";
   const { ProductSearcher } = searcherData;
   return (
-    <>
-      <InputText
-        type="text"
-        id="ProductSearcher"
-        label="Search a product"
-        value={ProductSearcher}
-        placeholder="Search a product"
-        handleChange={setSearcherWord}
-      />
-      <div>i'm Home</div>
-      <Link to={`/details/${id}`}>
-        <button>Go to details</button>
-      </Link>
-      <button onClick={products}>productlist</button>
-    </>
+    <Container>
+      <Row>
+        <Col xs={12} md={8} lg={8}></Col>
+        <Col xs={12} md={4} lg={4}>
+          <InputText
+            type="text"
+            id="ProductSearcher"
+            label="Search a product"
+            value={ProductSearcher}
+            placeholder="Search a product"
+            handleChange={setSearcherWord}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Link to={`/details/${id}`}>
+          <button>Go to details</button>
+        </Link>
+      </Row>
+    </Container>
   );
 }
 export default Home;
